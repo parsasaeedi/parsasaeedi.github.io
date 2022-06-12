632b7a0ae8815a5b4ff404230913dea7eac95d7e
Parsa <psaeedi1381@gmail.com>
2022-06-12 11:42:53 -0700

    Revert "The point at which I gave up on React"
    This reverts commit b5c89798c5eca5734652200cdbe996e565c04a85.

--- src/Home/main.js
+++
@@ -1,5 +0,0 @@
-// custom cursor
-// const cursor = document.getElementById('cursor');
-// window.onmousemove = (e) => {
-//     cursor.setAttribute('style', `top: ${e.pageY}px; left: ${e.pageX}px; z-index: 2;`)    
-// }
