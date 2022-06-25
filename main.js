073cc01ed242305b2c21ca9c0250962432799a8b
Parsa <psaeedi1381@gmail.com>
2022-06-25 16:19:04 -0700

    Fixed the cursor not moving with scroll

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // custom cursor
 const cursor = document.querySelector('.cursor');
 window.onmousemove = (e) => {
-    cursor.setAttribute('style', `top: ${e.pageY}px; left: ${e.pageX}px; z-index: 2;`)    
+    cursor.setAttribute('style', `top: ${e.clientY}px; left: ${e.clientX}px; z-index: 2;`)   
 }
