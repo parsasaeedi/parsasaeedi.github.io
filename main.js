0b537f01ee327e65b34e94d5aea2859fbad3367d
Parsa <psaeedi1381@gmail.com>
2022-06-25 18:54:49 -0700

    The cursor is more customized

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 // custom cursor
-const cursor = document.querySelector('.cursor');
+let cursor = document.querySelector('#cursor');
 window.onmousemove = (e) => {
-    cursor.setAttribute('style', `top: ${e.clientY}px; left: ${e.clientX}px; z-index: 2;`)   
+    cursor.setAttribute('style', `top: ${e.clientY}px; left: ${e.clientX}px; z-index: 2;`)  
 }
+
+let hoverableTexts = Array.from(document.querySelectorAll('.hoverableText'));
+hoverableTexts.forEach((text) => {
+    text.addEventListener("mouseover", () => {
+        cursor.classList.add("cursorHoverText")
+    });
+    text.addEventListener("mouseleave", () => {
+        cursor.classList.remove("cursorHoverText")
+    });
+})
+
+let links = Array.from(document.querySelectorAll('.link'));
+links.forEach((link) => {
+    link.addEventListener("mouseover", () => {
+        cursor.classList.add("linkHover")
+    });
+    link.addEventListener("mouseleave", () => {
+        cursor.classList.remove("linkHover")
+    });
+})
