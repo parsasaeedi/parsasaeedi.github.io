33aeae036069abed31d30f16b9de6438c14c1d04
Parsa <psaeedi1381@gmail.com>
2022-06-27 21:39:25 -0700

    Section titles now move with scrolling

--- main.js
+++ main.js
@@ -23,3 +23,75 @@ links.forEach((link) => {
         cursor.classList.remove("linkHover")
     });
 })
+
+// gsap.to(".experienceCard", {
+//     scrollTrigger: {
+//         trigger: ".experienceCard",
+//         toggleActions: "restart none none none none"
+//     },
+//     x: -200,
+//     duration: 1
+// });
+// gsap.from('.line1', { duration: 1, y: '-100%', ease: 'bounce' })
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".line1_1", {
+    scrollTrigger: {
+        trigger: ".line1_1",
+        toggleActions: "restart none none none",
+        scrub: true
+    },
+    transform: "translateX(-50vw)",
+    ease: "none"
+});
+
+gsap.from(".line2_1", {
+    scrollTrigger: {
+        trigger: ".line2_1",
+        toggleActions: "restart none none none",
+        scrub: true
+    },
+    transform: "translateX(-50vw)",
+    ease: "none"
+});
+
+gsap.to(".line1_2", {
+    scrollTrigger: {
+        trigger: ".line1_2",
+        toggleActions: "restart none none none",
+        scrub: true
+    },
+    transform: "translateX(-50vw)",
+    ease: "none"
+});
+
+gsap.from(".line2_2", {
+    scrollTrigger: {
+        trigger: ".line2_2",
+        toggleActions: "restart none none none",
+        scrub: true
+    },
+    transform: "translateX(-50vw)",
+    ease: "none"
+});
+
+gsap.to(".line1_3", {
+    scrollTrigger: {
+        trigger: ".line1_3",
+        toggleActions: "restart none none none",
+        scrub: true
+    },
+    transform: "translateX(-50vw)",
+    ease: "none"
+});
+
+gsap.from(".line2_3", {
+    scrollTrigger: {
+        trigger: ".line2_3",
+        toggleActions: "restart none none none",
+        scrub: true
+    },
+    transform: "translateX(-50vw)",
+    ease: "none"
+});
